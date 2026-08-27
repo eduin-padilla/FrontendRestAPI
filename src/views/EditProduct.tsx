902064de065ccf4b1cbc6250b53222b4976a87c2
Edwin Padilla <edwinpadillaorozco1@gmail.com>
2026-08-27 11:15:18 -0500

    Añadiendo funcionalidad para editar productos, incluyendo carga de datos y validación de campos

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,10 +1,33 @@
-import { Link, Form, useActionData, type ActionFunctionArgs, redirect} from "react-router-dom";
+import { Link, Form, useActionData, type ActionFunctionArgs, redirect, type LoaderFunctionArgs, useLoaderData} from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
-import { addProduct } from "../services/ProductService";
+import { getProductById, updateProduct } from "../services/ProductService";
+import type { Product } from "../types";
+
 
 
 // eslint-disable-next-line react-refresh/only-export-components
-export async function action({request}: ActionFunctionArgs) {
+export async function loader({params}: LoaderFunctionArgs) {
+  if(params.id !== undefined){
+
+    const product = await getProductById(+params.id)
+      if(!product){
+         //throw new Response('', {status: 404, statusText: 'No encontrado'})
+
+         return redirect('/')
+      }
+      return product
+  }
+
+  return{
+    
+  }
+
+
+  
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function action({request, params}: ActionFunctionArgs) {
 
   const data = Object.fromEntries(await request.formData())
 
@@ -16,25 +39,34 @@ export async function action({request}: ActionFunctionArgs) {
   if(error.length){
     return error
   }
-  await addProduct(data)
 
+  if(params.id !== undefined){
+
+    await updateProduct(data, +params.id)
+  }
 
   return redirect('/')
 
 }
 
+const availabilityOptions = [
+   { name: 'Disponible', value: true},
+   { name: 'No Disponible', value: false}
+]
+
+
+
 
 export default function EditProduct() {
 
+  const product = useLoaderData() as Product
+
   const error = useActionData()  as string
-
-
-
 
   return (
     <>
       <div className='flex justify-between'> 
-            <h2 className='text-4xl font-black text-slate-500'>Registrar Producto</h2>
+            <h2 className='text-4xl font-black text-slate-500'>Editar Producto</h2>
             <Link to={"/"} 
             className='rounded-md bg-indigo-600  p-3 hover:bg-indigo-500 text-white font-bold py-2 px-4 shadow-md'>
                 Volver a Productos
@@ -54,6 +86,7 @@ export default function EditProduct() {
                   className="mt-2 block w-full p-3 bg-gray-50"
                   placeholder="Nombre del Producto"
                   name="name"
+                  defaultValue={product.name}
               />
           </div>
           <div className="mb-4">
@@ -67,13 +100,31 @@ export default function EditProduct() {
                   className="mt-2 block w-full p-3 bg-gray-50"
                   placeholder="Precio Producto. ej. 200, 300"
                   name="price"
+                  defaultValue={product.price}
               />
+          </div>
+
+          <div className="mb-4">
+              <label
+                  className="text-gray-800"
+                  htmlFor="availability"
+              >Disponibilidad:</label>
+              <select 
+                  id="availability"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  name="availability"
+                  defaultValue={product?.availability.toString()}
+              >
+                  {availabilityOptions.map(option => (
+                    <option key={option.name} value={option.value.toString()}>{option.name}</option>
+                  ))}
+              </select>
           </div>
           
           <input
             type="submit"
             className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-            value="Registrar Producto"
+            value="Editar Producto"
           />
         </Form>
     </>
