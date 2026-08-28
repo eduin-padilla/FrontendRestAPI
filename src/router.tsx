96cf045863b7ac619b340ab17062fb8ec22e8ef4
Edwin Padilla <edwinpadillaorozco1@gmail.com>
2026-08-28 13:10:08 -0500

    Añadiendo la ruta para eliminar el producto

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import Layout from './layout/Layout'
 import Products, {loader as productsLoader} from './views/Products'
 import NewProduct, {action as newProductAction} from './views/NewProduct'
 import EditProduct, {loader as editProduct, action as editProductAction} from './views/EditProduct'
+import { action as deleteProductAction } from './components/ProductDetails'
 
 
 
@@ -27,6 +28,10 @@ export const router = createBrowserRouter([
                 element: <EditProduct/>,
                 loader: editProduct,
                 action: editProductAction
+            },
+            {
+                path: 'productos/:id/eliminar',
+                action: deleteProductAction
             }
         ],
     }
