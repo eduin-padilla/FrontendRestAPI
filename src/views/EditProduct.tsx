9c0a0360568b6d61800f0f82346a05c29f85aa5b
Edwin Padilla <edwinpadillaorozco1@gmail.com>
2026-08-28 10:50:00 -0500

    Creando componente para reutilizar el formulario

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -2,6 +2,7 @@ import { Link, Form, useActionData, type ActionFunctionArgs, redirect, type Load
 import ErrorMessage from "../components/ErrorMessage";
 import { getProductById, updateProduct } from "../services/ProductService";
 import type { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 
 
 
@@ -75,34 +76,9 @@ export default function EditProduct() {
         {error && <ErrorMessage>{error}</ErrorMessage>}
         <Form className="mt-10" method="POST"> 
       
-          <div className="mb-4">
-              <label
-                  className="text-gray-800"
-                  htmlFor="name"
-              >Nombre Producto:</label>
-              <input 
-                  id="name"
-                  type="text"
-                  className="mt-2 block w-full p-3 bg-gray-50"
-                  placeholder="Nombre del Producto"
-                  name="name"
-                  defaultValue={product.name}
-              />
-          </div>
-          <div className="mb-4">
-              <label
-                  className="text-gray-800"
-                  htmlFor="price"
-              >Precio:</label>
-              <input 
-                  id="price"
-                  type="number"
-                  className="mt-2 block w-full p-3 bg-gray-50"
-                  placeholder="Precio Producto. ej. 200, 300"
-                  name="price"
-                  defaultValue={product.price}
-              />
-          </div>
+          <ProductForm
+            product= {product}
+          />
 
           <div className="mb-4">
               <label
--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,6 +1,7 @@
 import { Link, Form, useActionData, type ActionFunctionArgs, redirect} from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductService";
+import ProductForm from "../components/ProductForm";
 
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -43,32 +44,7 @@ export default function NewProduct() {
         {error && <ErrorMessage>{error}</ErrorMessage>}
         <Form className="mt-10" method="POST"> 
       
-          <div className="mb-4">
-              <label
-                  className="text-gray-800"
-                  htmlFor="name"
-              >Nombre Producto:</label>
-              <input 
-                  id="name"
-                  type="text"
-                  className="mt-2 block w-full p-3 bg-gray-50"
-                  placeholder="Nombre del Producto"
-                  name="name"
-              />
-          </div>
-          <div className="mb-4">
-              <label
-                  className="text-gray-800"
-                  htmlFor="price"
-              >Precio:</label>
-              <input 
-                  id="price"
-                  type="number"
-                  className="mt-2 block w-full p-3 bg-gray-50"
-                  placeholder="Precio Producto. ej. 200, 300"
-                  name="price"
-              />
-          </div>
+          <ProductForm/>
           
           <input
             type="submit"
