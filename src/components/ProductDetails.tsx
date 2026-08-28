eb6c3f1aa88b1cd76c461bae5d195a49d20d72c1
Edwin Padilla <edwinpadillaorozco1@gmail.com>
2026-08-28 13:10:55 -0500

    Añadiendo funcionalidad para eliminar productos

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,10 +1,23 @@
-import { Link } from "react-router-dom"
+import { Form, Link, type ActionFunctionArgs, redirect } from "react-router-dom"
 import { type Product } from "../types"
 import { formatCurrency } from "../util"
+import { deleteProduct } from "../services/ProductService"
 
 type roductDetailPros = {
     product : Product
 }
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function action({params}: ActionFunctionArgs) {
+
+    if(params.id !== undefined){
+
+        await deleteProduct(+params.id)
+        return redirect('/')
+    }
+    
+    
+ }
 
 export default function ProductDetails({product}: roductDetailPros) {
 
@@ -19,11 +32,35 @@ export default function ProductDetails({product}: roductDetailPros) {
             {formatCurrency(product.price)}
         </td>
         <td className="p-3 text-lg text-gray-800 text-center">
-            {isAvailability ? 'Disponible' : 'No disponible' }
+            <form method="POST">
+                <button type='button' name="availability" value={product.availability.toString()} 
+                className={`${isAvailability ? 'text-black' : 'text-red-600' } rounded-lg p-2 text-xs uppercase font-bold w-full border  border-black hover:cursor-pointer`}>
+                    {isAvailability ? 'Disponible' : 'No disponible' }  
+                </button>
+            </form>
         </td>
         <td className="p-3 text-lg text-gray-800  text-center">
            <div className="flex gap-2 items-center">
+
+
             <Link to={`productos/${product.id}/editar`} className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center">Editar</Link>
+
+
+            <Form className="w-full" method="POST" 
+            action={`productos/${product.id}/eliminar`}
+            onSubmit={(e) => {
+                if(!confirm('Eliminar?')){
+                    e.preventDefault()
+                }
+            }}
+            >
+
+                <input type="submit" 
+                className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                value='Eliminar'
+                />
+            </Form>
+            
            </div>
         </td>
     </tr> 
--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -98,3 +98,16 @@ export async function updateProduct(data: ProductData, id: Product['id']) {
         
     }
 }
+
+export async function deleteProduct(id: Product['id']) {
+
+    try {
+        const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+
+        await axios.delete(url)
+    } catch (error) {
+        console.log(error)
+        
+    }
+    
+}
